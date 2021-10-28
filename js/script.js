99aa7ab3d2b5ad32461e5478a8c00d32463b73e7
unknown <zztrunghungzz@gmail.com>
2021-10-28 17:20:35 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    //Menu
+    $(".menu-icon").click(function (e) { 
+        e.preventDefault();
+        $(".menu-bar").addClass("active");
+        $(".cover").addClass("active");
+    });
+    $(".cover").click(function (e) { 
+        e.preventDefault();
+        $(".menu-bar").removeClass('active');
+        $(".cover").removeClass('active');
+    });
+    $(".menu-close").click(function (e) { 
+        e.preventDefault();
+        $(".menu-bar").removeClass('active');
+        $(".cover").removeClass('active');
+    });
+    //Scroll menu 
+    $(window).scroll(function () { 
+        if ($(window).scrollTop()) {
+            $('.header').css('background-color','rgba(0, 0, 0, 1)');
+        } else {
+            $('.header').css('background-color','rgba(0, 0, 0, 0.6)');
+        }
+    });
+
+    // Slick slider
+    $('.slider').slick({
+        arrows: false,
+    });
+    // Slick client
+    $('.client__list').slick({
+        dots: true,
+        arrows: false,
+    });
+});
